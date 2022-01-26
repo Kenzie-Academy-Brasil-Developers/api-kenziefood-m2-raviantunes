f9928c34e2c47d5a7bcdaac8aa4ee3ffa39e8db9
anavaleska2908 <avaleska2908@hotmail.com>
2022-01-26 20:40:53 -0300

    fix: métodos da classe filter recebendo informações da API para realizar os filtros e zerando o HTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,30 @@
 import { filter } from './Filter.js';
-import { precoCarrinho } from './PrecoCarrinho.js';
-import { quantidadeProdutos } from './QuantidadeProdutos.js';
-import { Template } from '../api-base/models/template';
+// import { precoCarrinho } from './PrecoCarrinho.js';
+// import { quantidadeProdutos } from './QuantidadeProdutos.js';
+import { Template } from '../api-base/models/template.js';
+import { buscarProdutos } from './Filter.js';
+import { filtrarProdutos } from './Filter.js';
 
 const botaoMostrarPanificadora = document.querySelector(
-  'filtros__button--panificadora'
+  '.filtros__button--panificadora'
 );
-botaoMostrarPanificadora.addEventListener(
-  'click',
-  filter.filtrarPorPanificadora
-);
+botaoMostrarPanificadora.addEventListener('click', filtrarProdutos);
 
 const botaoMostrarFrutas = document.querySelector('.filtros__button--frutas');
-botaoMostrarFrutas.addEventListener('click', () => {
-  filter.filtrarPorFrutas(produtoFiltragem);
-});
+botaoMostrarFrutas.addEventListener('click', filtrarProdutos);
 
 const botaoMostrarBebidas = document.querySelector('.filtros__button--bebidas');
-botaoMostrarBebidas.addEventListener('click', () => {
-  filter.filtrarPorBebidas(produtoFiltragem);
-});
+botaoMostrarBebidas.addEventListener('click', filtrarProdutos);
 
 const inputCampoBusca = document.querySelector('.pesquisa__input');
 inputCampoBusca.addEventListener('onKeyUp', () => {
-  filter.filtrarPorNomeBuscado(produtoFiltragem);
+  filter.filtrarPorNomeBuscado(inputCampoBusca.value);
 });
 
 const botaoCampoBusca = document.querySelector('.pesquisa__button');
-botaoCampoBusca.addEventListener('click', () => {
-  filter.filtrarPorNomeBuscado(produtoFiltragem);
-});
+botaoCampoBusca.addEventListener('click', filtrarProdutos);
 
 const botaoMostrarTodos = document.querySelector('.filtros__button--todos');
 botaoMostrarTodos.addEventListener('click', () => {
-  Template.produto(produtoFiltragem);
+  getProducts();
 });
