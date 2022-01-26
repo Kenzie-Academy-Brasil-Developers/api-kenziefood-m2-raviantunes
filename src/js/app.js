ef044f36b7323cef1144875a45effa51ae66d8bc
anavaleska2908 <avaleska2908@hotmail.com>
2022-01-26 15:16:41 -0300

    fix: métodos da classe filter recebendo objetos e descontruindo dentro do método filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import { filter } from './Filter.js';
 import { precoCarrinho } from './PrecoCarrinho.js';
 import { quantidadeProdutos } from './QuantidadeProdutos.js';
-import { Template } from '../api-base/models/template'
+import { Template } from '../api-base/models/template';
 
 const botaoMostrarPanificadora = document.querySelector(
   'filtros__button--panificadora'
@@ -12,14 +12,26 @@ botaoMostrarPanificadora.addEventListener(
 );
 
 const botaoMostrarFrutas = document.querySelector('.filtros__button--frutas');
-botaoMostrarFrutas.addEventListener('click', filter.filtrarPorFrutas);
-
-const botaoMostrarBebidas = document.querySelector('.filtros__button--bebidas');
-botaoMostrarBebidas.addEventListener('click', filter.filtrarPorBebidas);
-
-const botaoMostrarTodos = document.querySelector('.filtros__button--todos');
-
-botaoMostrarTodos.addEventListener('click', () => {
-  Template.produto(produto);
+botaoMostrarFrutas.addEventListener('click', () => {
+  filter.filtrarPorFrutas(produtoFiltragem);
 });
 
+const botaoMostrarBebidas = document.querySelector('.filtros__button--bebidas');
+botaoMostrarBebidas.addEventListener('click', () => {
+  filter.filtrarPorBebidas(produtoFiltragem);
+});
+
+const inputCampoBusca = document.querySelector('.pesquisa__input');
+inputCampoBusca.addEventListener('onKeyUp', () => {
+  filter.filtrarPorNomeBuscado(produtoFiltragem);
+});
+
+const botaoCampoBusca = document.querySelector('.pesquisa__button');
+botaoCampoBusca.addEventListener('click', () => {
+  filter.filtrarPorNomeBuscado(produtoFiltragem);
+});
+
+const botaoMostrarTodos = document.querySelector('.filtros__button--todos');
+botaoMostrarTodos.addEventListener('click', () => {
+  Template.produto(produtoFiltragem);
+});
