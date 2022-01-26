af3f252636e62bca97b6002b07dc76dfed846266
Guilherme <guilherme.llopreti@gmail.com>
2022-01-26 09:52:56 -0300

    fix: alteração no nome da classe chamada para alterar quantidade no carrinho

--- src/js/QuantidadeProdutos.js
+++ src/js/QuantidadeProdutos.js
@@ -1,9 +1,8 @@
 export { quantidadeProdutos };
 const quantidadeProdutos = class QuantidadeProdutos {
   static quantidadeProdutosCarrinho(produtos) {
-    const pQuantidadeProdutos = document.querySelector(
-      '.valorQuantidadeProdutos'
-    );
+    const pQuantidadeProdutos = document.querySelector('.quantidade__valor');
+
     pQuantidadeProdutos.innerText = `${produtos.length}`;
   }
 };
